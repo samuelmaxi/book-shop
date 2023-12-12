8196da13cf03decb064873b63cb51fd8d5bc1a50
Samuel Máximo <samuelgaraujo87@gmail.com>
2023-12-12 16:58:49 -0300

    feat: add styles for pages

--- front-book-shop/src/app/page.tsx
+++ front-book-shop/src/app/page.tsx
@@ -1,9 +1,35 @@
+"use client"
+
 import Link from "next/link";
+import React from "react";
+
+
+type PageProps={
+    children: React.ReactNode,
+    tag: any 
+}
+
+const Title = ({children, tag}: PageProps)=>{
+  const Tag = tag
+  return(
+    <React.Fragment>
+    <Tag>
+      {children}
+    </Tag>
+
+    <style jsx>{`
+    ${Tag}{
+      color: red;
+    }
+    `}</style>
+    </React.Fragment>
+  )
+}
 
 export default function Page() {
   return (
     <div>
-
+      <Title tag="h1">Home Page</Title>
       <Link href={"/faq"}>FAQ</Link>
     </div>
   )
