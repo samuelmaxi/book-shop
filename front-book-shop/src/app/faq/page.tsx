96d7a2aacdc58aa001e711900242181870fbe777
Samuel Máximo <samuelgaraujo87@gmail.com>
2023-12-12 17:00:28 -0300

    feat: add page FAQ

---
+++ front-book-shop/src/app/faq/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function FAQPage(){
+  return(
+    <div>
+      <h1>FAQ</h1>
+      <Link href={"/"}>Voltar para o Home</Link>
+    </div>
+  );
+};
