7cfd8b6472cb340667b61f9e63a10db254d85ea8
Samuel Máximo <samuelgaraujo87@gmail.com>
2024-01-15 16:21:39 -0300

    feat: add component Card

--- front-book-shop/src/app/page.tsx
+++ front-book-shop/src/app/page.tsx
@@ -1,12 +1,15 @@
 import Card from '@/components/Card'
 import styles from './page.module.css'
+import GalleryCard from '@/components/GalleryCard'
 
 export default function Home() {
+
+
   return (
     <main className={styles.main}>
       <h1>Hello word!</h1>
+      <GalleryCard />
 
-      <Card />
     </main>
   )
 }
--- front-book-shop/src/components/Card/index.tsx
+++ front-book-shop/src/components/Card/index.tsx
@@ -1,30 +1,38 @@
-import Image from "next/image"
+"use client"
+import Image, { StaticImageData } from "next/image"
 import styles from "./styles.module.css"
 import img from "../../../public/img/1.png"
 
-export async function getServerSideProps() {
-  const res = await fetch("https://api.lorem.space/image/movie?w=150&amp;amp;amp;amp;h=220")
-  const images = await res.json
-
-  return { props: { images } }
+type CardProps = {
+  image: {
+    imgData: StaticImageData | string,
+    width: number | `${number}`,
+    height: number | `${number}`,
+  },
+  title: string,
+  paragraph: string,
 }
 
-const Card = () => {
+const Card = ({ image, paragraph, title }: CardProps) => {
+
+  function handlerCard() {
+
+  }
 
   return (
     <section className={styles.container}>
       <section className={styles.image}>
-        <Image src={img} width={100} height={100} alt="imagem de capa de livro~" />
+        <Image src={image.imgData} width={image.width} height={image.height} alt="imagem de capa de livro~" />
       </section>
       <section className={styles.sectionText}>
         <section className={styles.sectionTextTitle}>
-          <h1>Book Name</h1>
+          <h1>{title}</h1>
         </section>
         <section className={styles.sectionTextParagraph}>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis vel, amet repellat aspernatur, cumque ipsa natus atque quisquam, blanditiis dignissimos praesentium! Laborum ullam saepe hic similique, ipsam corporis! Similique, incidunt.</p>
+          <p>{paragraph}</p>
         </section>
       </section>
-
+      <button onClick={handlerCard}>ADD</button>
     </section>
   )
 }
